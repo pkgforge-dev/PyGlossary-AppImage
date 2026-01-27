a4b2627589f17ca084c9a10a1dd098b0a324fa59
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-27 18:36:09 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,23 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q pyglossary | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/pyglossary.svg
+export DESKTOP=/usr/share/applications/pyglossary.desktop
+export DEPLOY_SYS_PYTHON=1
+export DEPLOY_GTK=1
+export GTK_DIR=gtk-3.0
+export DEPLOY_LOCALE=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/pyglossary*   \
+	/usr/share/pyglossary  \
+	/usr/lib/libgirepository*
 
 # Additional changes can be done in between here
 
